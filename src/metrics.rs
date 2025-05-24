f0ec5d2fda69266fbf25d2236b428940e65681ed
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2025-05-24 23:06:46 +0800

    feature: support DashMap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,15 +1,13 @@
 // metrics data structure
 // basement: inc/dec/snapshot
 use anyhow::Result;
-use std::{
-    collections::HashMap,
-    fmt,
-    sync::{Arc, RwLock},
-};
+use std::{fmt, sync::Arc};
+
+use dashmap::DashMap;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Default for Metrics {
@@ -21,36 +19,23 @@ impl Default for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self
-            .data
-            .write()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        data.entry(key.into())
+        self.data
+            .entry(key.into())
             .and_modify(|count| *count += 1)
             .or_insert(1);
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?
-            .clone())
     }
 }
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let data = self.data.read().map_err(|_e| fmt::Error {})?;
-        for (key, count) in data.iter() {
-            writeln!(f, "{} {}", key, count)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}, {}", entry.key(), entry.value())?;
         }
         Ok(())
     }
